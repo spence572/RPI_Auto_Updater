39de3fcafcad679b4b0407063c84b7ed802f56e2
Paul Spencer <pauldsp@outlook.com>
2017-05-13 01:47:02 +0100

    Added runtime check to ensure you haven’t ran this before

--- install_updater.sh
+++ install_updater.sh
@@ -2,16 +2,21 @@
 # Created By:	Paul Spencer based on http://www.instructables.com/id/Raspberry-Pi-Auto-Update/ by JaredT22
 # Description:	File to configure the auto update for the RPI.
 
+if [[ -d updater ]]
+	then
+		echo "Looks like you have already ran this script. Please * rm -r updater * then rerun this"
+		exit 0
+fi
 
 cd ~
-sudo mkdir updater
-sudo mkdir updater/logs
+mkdir updater
+mkdir updater/logs
 touch updater/logs/cronlog
 chmod -R 777 updater
 cd updater
 
 #Create the update.sh script
-sudo touch update.sh
+touch update.sh
 
 echo "#!/bin/sh" >> update.sh
 echo "sudo apt-get update && sudo apt-get upgrade -y" >> update.sh
@@ -20,6 +25,6 @@ echo "sudo apt-get autoremove" >> update.sh
 echo "sudo apt-get autoclean" >> update.sh
 echo "sudo reboot" >> update.sh
 
-sudo chmod +x update.sh
+chmod +x update.sh
 
 (crontab -l 2>/dev/null; echo "0 0 * * SAT /home/pi/updater/update.sh 2>/home/pi/updater/logs/cronlog") | crontab -
